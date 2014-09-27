7fd7f40b564622e5b2142cab01ed7d81e2fe2295
djluo <dj.luo@baoyugame.com>
2014-09-27 16:02:16 +0800

    Independent of path

--- run.sh
+++ run.sh
@@ -26,12 +26,12 @@ run() {
   local cmd="$@"
   [ -d "$cid" ] || mkdir "${cid}"
 
-  sudo docker run $mode $cidfile $port  \
-      -v `pwd`/conf:`pwd`/conf/         \
-      -v `pwd`/logs/:`pwd`/logs/        \
-      -v `pwd`/html/:`pwd`/html/        \
-      -e "TZ=Asia/Shanghai"             \
-      --name ${app} apache              \
+  sudo docker run $mode $cidfile $port \
+      -v ${current_dir}/conf/:${current_dir}/conf/ \
+      -v ${current_dir}/logs/:${current_dir}/logs/ \
+      -v ${current_dir}/html/:${current_dir}/html/ \
+      -e "TZ=Asia/Shanghai" \
+      --name ${app} apache  \
       $cmd
 }
 usage() {
@@ -42,7 +42,7 @@ usage() {
 case "$1" in
   start)
     if [ ! -f "$cid/$app.cid" ];then
-      run --cidfile="$cid/$app.cid" /usr/sbin/httpd -DFOREGROUND -f `pwd`/conf/httpd.conf
+      run --cidfile="$cid/$app.cid" /usr/sbin/httpd -DFOREGROUND -f ${current_dir}/conf/httpd.conf
     else
       sudo docker start $app
     fi
